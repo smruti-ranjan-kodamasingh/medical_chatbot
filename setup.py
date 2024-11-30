cc4542dd0998c2663a4e6572dd49d636f82f9c1f
smruti-2003 <srsingh2678@gmail.com>
2024-11-30 08:54:29 +0530

    packages installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen-AI Project',
+    version= '0.0.0',
+    author= 'smruti ranjan',
+    author_email= 'srsingh2678@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
